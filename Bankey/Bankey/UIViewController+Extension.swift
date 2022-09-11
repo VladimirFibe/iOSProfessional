f6b0a67e9ca792caf7df743cdbb3e932f59ed7e1
Vladimir <motiw@icloud.com>
2022-09-11 23:51:33 +0600

    UINavigationBarAppearance

--- Bankey/Bankey/UIViewController+Extension.swift
+++ Bankey/Bankey/UIViewController+Extension.swift
@@ -9,12 +9,12 @@ import UIKit
 
 extension UIViewController {
   func setStatusBar() {
-    let statusBarSize = UIApplication.shared.statusBarFrame.size // deprecated
     
-    let frame = CGRect(origin: .zero, size: statusBarSize)
-    let statusbarView = UIView(frame: frame)
-    statusbarView.backgroundColor = .appColor
-    view.addSubview(statusbarView)
+    let navBarAppearance = UINavigationBarAppearance()
+    navBarAppearance.configureWithOpaqueBackground()
+    navBarAppearance.backgroundColor = .appColor
+    UINavigationBar.appearance().standardAppearance = navBarAppearance
+    UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
   }
   
   func setTabBarImage(imageName: String, title: String) {
