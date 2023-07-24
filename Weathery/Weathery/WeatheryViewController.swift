a95db4cb027d79f8b0b61bb422dc77e28b3aefd2
Vladimir Fibe <motiw@icloud.com>
2023-07-24 15:14:07 +0600

    Layout

--- Weathery/Weathery/WeatheryViewController.swift
+++ Weathery/Weathery/WeatheryViewController.swift
@@ -3,8 +3,10 @@ import UIKit
 class WeatheryViewController: UIViewController {
 
     private let backgroundView = UIImageView()
-    private let searchTextField = UITextField()
+    private let rootStackView = UIStackView()
+    private let searchStackView = UIStackView()
     private let locationButton = UIButton(type: .system)
+    private let searchTextField = UITextField()
     private let searchButton = UIButton(type: .system)
     private let conditionImageView = UIImageView()
     private let temperatureLabel = UILabel()
@@ -29,9 +31,11 @@ extension WeatheryViewController {
 extension WeatheryViewController {
     private func setupViews() {
         setupBackgroundView()
+        setupRootStackView()
+        setupSearchStackView()
         setupLocationButton()
-        setupSearchButton()
         setupSearchTextField()
+        setupSearchButton()
         setupConditionImageView()
         setupTemperatureLabel()
         setupCityLabel()
@@ -51,59 +55,117 @@ extension WeatheryViewController {
         ])
     }
     
+    private func setupRootStackView() {
+        view.addSubview(rootStackView)
+        rootStackView.translatesAutoresizingMaskIntoConstraints = false
+        rootStackView.axis = .vertical
+        rootStackView.spacing = 10
+        rootStackView.alignment = .trailing
+        
+        NSLayoutConstraint.activate([
+            rootStackView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            rootStackView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            rootStackView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        ])
+    }
+    
+    private func setupSearchStackView() {
+        rootStackView.addArrangedSubview(searchStackView)
+        searchStackView.translatesAutoresizingMaskIntoConstraints = false
+        searchStackView.axis = .horizontal
+        searchStackView.spacing = 10
+        searchStackView.widthAnchor.constraint(equalTo: rootStackView.widthAnchor).isActive = true
+    }
+    
     private func setupLocationButton() {
-        view.addSubview(locationButton)
+        searchStackView.addArrangedSubview(locationButton)
         locationButton.translatesAutoresizingMaskIntoConstraints = false
         locationButton.setBackgroundImage(UIImage(systemName: "location.north.circle.fill"), for: [])
         locationButton.tintColor = .label
         locationButton.addTarget(self, action: #selector(didTapLocationButton), for: .primaryActionTriggered)
         NSLayoutConstraint.activate([
-            locationButton.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
-            locationButton.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
             locationButton.widthAnchor.constraint(equalToConstant: 44),
             locationButton.heightAnchor.constraint(equalToConstant: 44)
         ])
     }
     
-    private func setupSearchButton() {
-        view.addSubview(searchButton)
-        searchButton.translatesAutoresizingMaskIntoConstraints = false
-        searchButton.setBackgroundImage(UIImage(systemName: "magnifyingglass"), for: [])
-        searchButton.tintColor = .label
-        searchButton.addTarget(self, action: #selector(didTapSearchButton), for: .primaryActionTriggered)
-        NSLayoutConstraint.activate([
-            searchButton.topAnchor.constraint(equalTo: locationButton.topAnchor),
-            searchButton.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-            searchButton.widthAnchor.constraint(equalTo: locationButton.widthAnchor),
-            searchButton.heightAnchor.constraint(equalTo: locationButton.heightAnchor)
-        ])
-    }
-    
     private func setupSearchTextField() {
-        view.addSubview(searchTextField)
+        searchStackView.addArrangedSubview(searchTextField)
         searchTextField.translatesAutoresizingMaskIntoConstraints = false
         searchTextField.placeholder = "Search"
         searchTextField.font = .preferredFont(forTextStyle: .title1)
         searchTextField.textAlignment = .right
         searchTextField.borderStyle = .roundedRect
         searchTextField.backgroundColor = .systemFill
+    }
+    
+    private func setupSearchButton() {
+        searchStackView.addArrangedSubview(searchButton)
+        searchButton.translatesAutoresizingMaskIntoConstraints = false
+        searchButton.setBackgroundImage(UIImage(systemName: "magnifyingglass"), for: [])
+        searchButton.tintColor = .label
+        searchButton.addTarget(self, action: #selector(didTapSearchButton), for: .primaryActionTriggered)
         NSLayoutConstraint.activate([
-            searchTextField.topAnchor.constraint(equalTo: locationButton.topAnchor),
-            searchTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: locationButton.trailingAnchor, multiplier: 1),
-            searchButton.leadingAnchor.constraint(equalToSystemSpacingAfter: searchTextField.trailingAnchor, multiplier: 1)
+            searchButton.widthAnchor.constraint(equalTo: locationButton.widthAnchor),
+            searchButton.heightAnchor.constraint(equalTo: locationButton.heightAnchor)
         ])
-        
     }
     
     private func setupConditionImageView() {
-        
+        rootStackView.addArrangedSubview(conditionImageView)
+        conditionImageView.image = UIImage(systemName: "cloud.bolt")
+        conditionImageView.tintColor = .label
+        NSLayoutConstraint.activate([
+            conditionImageView.widthAnchor.constraint(equalToConstant: 120),
+            conditionImageView.heightAnchor.constraint(equalToConstant: 120)
+        ])
     }
     
     private func setupTemperatureLabel() {
-        
+        rootStackView.addArrangedSubview(temperatureLabel)
+        temperatureLabel.attributedText = makeTemperatureText(with: "22")
     }
     
     private func setupCityLabel() {
-        
+        rootStackView.addArrangedSubview(cityLabel)
+        cityLabel.text = "San Francisco"
+        cityLabel.font = .preferredFont(forTextStyle: .largeTitle)
+    }
+    
+    private func makeTemperatureText(with temperature: String) -> NSAttributedString {
+        let boldTextAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.label,
+            .font: UIFont.boldSystemFont(ofSize: 100)
+        ]
+        let plainTextAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 80)
+        ]
+        let text = NSMutableAttributedString(string: temperature, attributes: boldTextAttributes)
+        text.append(NSAttributedString(string: "ºC", attributes: plainTextAttributes))
+        return text
+    }
+    
+    func makeAttributed2() -> NSAttributedString {
+        let dollarSignAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.preferredFont(forTextStyle: .callout),
+            .baselineOffset: 8]
+        let priceAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.preferredFont(forTextStyle: .title1)]
+        let monthAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.preferredFont(forTextStyle: .callout)]
+        let termAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.preferredFont(forTextStyle: .footnote),
+                .baselineOffset: 8]
+
+        let rootString = NSMutableAttributedString(string: "$", attributes: dollarSignAttributes)
+        let priceString = NSAttributedString(string: "8", attributes: priceAttributes)
+        let monthString = NSAttributedString(string: "/mo", attributes: monthAttributes)
+        let termString = NSAttributedString(string: "1", attributes: termAttributes)
+
+        rootString.append(priceString)
+        rootString.append(monthString)
+        rootString.append(termString)
+
+        return rootString
     }
 }
