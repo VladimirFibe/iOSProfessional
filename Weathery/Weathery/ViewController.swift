9999f0b5d6bad918e03b5abef9cd2f869c33a656
Vladimir Fibe <motiw@icloud.com>
2023-07-24 08:16:22 +0600

    remove storyboard

--- Weathery/Weathery/ViewController.swift
+++ Weathery/Weathery/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  Weathery
-//
-//  Created by MacService on 7/24/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
     }
 
 
